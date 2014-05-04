21cd20bb4e57f46303ee80744a15831a9365e22f
gbuomprisco <giancarlopsk@gmail.com>
2014-05-04 22:47:34 +0100

    Updated README, added Javascript version

---
+++ Javascript/pattern-matching.js
@@ -0,0 +1,97 @@
+/*
+ ** Compute borders of a string
+ */
+
+function Borders(x, m) {
+    var i, j = 1;
+    var border = [];
+    while (j < m) {
+        border.push(i);
+        while (i >= 0 && x[j] !== x[i]) {
+            if (!i) {
+                i--;
+            } else {
+                i = border[i - 1];
+            }
+        }
+        i++;
+        j++;
+    }
+    border.push(i);
+    return border;
+}
+
+/*
+ ** Preprocessing Morris-Pratt Algorithm
+ */
+
+function MP_Next(x, m) {
+    var j = -1,
+        i = 0;
+    var mpNext = [j];
+    while (i < m - 1) {
+        while (j >= 0 && x[i] !== x[j]) {
+            j = mpNext[j];
+        }
+        j++;
+        i++;
+        mpNext.push(j);
+    }
+    mpNext.push(j);
+    return mpNext;
+}
+
+/*
+ ** Preprocessing Knuth-Morris-Pratt Algorithm
+ */
+
+function KMP_Next(x, m) {
+    var j = -1,
+        i = 0;
+    var kmpNext = [j];
+    while (i < m - 1) {
+        while (j >= 0 && x[i] !== x[j]) {
+            j = mpNext[j];
+        }
+        j++;
+        i++;
+        if (x[i] == x[j]) {
+            kmpNext.push(kmpNext[j]);
+        } else {
+            kmpNext.push(j);
+        }
+        return kmpNext;
+    }
+}
+
+/*
+ ** KMP and MP Algorithms Search function
+ */
+
+
+function Search(x, y, m, n, algorithm) {
+
+    var borders;
+
+    if (algorithm == 'kmp') {
+        borders = KMP_Next(x, m);
+    } else if (algorithm == 'mp') {
+        borders = MP_Next(x, m);
+    }
+
+    var i = 0,
+        j = 0;
+
+    while (j < m) {
+        while (i == n || (i >= 0 && x[j] != y[i])) {
+            i = borders[i];
+        }
+
+        i++;
+        j++;
+
+        if (i == n) {
+            console.log("x occurs in y at the position " + (j - 1));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Pattern-Matching-Algorithms
 ===========================
 
 Pattern Matching Algorithms written by using different scripting languages
+
+## Reference
+
+1. http://www-igm.univ-mlv.fr/~lecroq/string/
+2. Crochemore, Maxime, Wojciech Rytter. Text Algorithms.
